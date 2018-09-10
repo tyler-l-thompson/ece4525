534fd1da4ee5824c1e0aafbd10a93f562b8a77b9
root <root@linux.wmich.edu>
2018-09-10 10:52:29 -0400

    Initial commit

---
+++ Lab2/Lab2.srcs/sources_1/new/Basic_Binary_Adder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/10/2018 10:45:44 AM
+// Design Name: 
+// Module Name: Basic_Binary_Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Basic_Binary_Adder(
+
+    );
+endmodule
