b5dadacc90efcf4326eed00ef5f4e9628f74152c
tyler <tyler.l.thompson@wmich.edu>
2018-09-12 12:23:47 -0400

    added hw2

---
+++ HW2/HW2.srcs/sources_1/new/Four_Bit_Magnitude_Comparator.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/12/2018 12:22:44 PM
+-- Design Name: 
+-- Module Name: Four_Bit_Magnitude_Comparator - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Four_Bit_Magnitude_Comparator is
+--  Port ( );
+end Four_Bit_Magnitude_Comparator;
+
+architecture Behavioral of Four_Bit_Magnitude_Comparator is
+
+begin
+
+
+end Behavioral;
