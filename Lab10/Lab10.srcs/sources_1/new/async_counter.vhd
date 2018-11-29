4350e38b9221464ef19a7db6ee629077bdf96efd
tyler <tyler.l.thompson@wmich.edu>
2018-11-28 19:12:40 -0500

    post lab10

--- Lab10/Lab10.srcs/sources_1/new/async_counter.vhd
+++ Lab10/Lab10.srcs/sources_1/new/async_counter.vhd
@@ -12,7 +12,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity async_counter is
     Port ( 
         CKA, R0, R5 : in STD_LOGIC;
-        Y1, Y2, Y3 : inout STD_LOGIC;
+        Y1, Y2, Y3, Y4 : inout STD_LOGIC;
         QA, QB, QC : out STD_LOGIC
     );
 end async_counter;
@@ -21,16 +21,46 @@ architecture Behavioral of async_counter is
 
 begin
 
-Y1 <= ((Y2 and not Y3 and not CKA) or (Y1 and Y2 and CKA) or (Y1 and Y2 and not Y3));
-
-Y2 <= ((Y1 and Y2) or (Y2 and CKA) or (Y2 and not Y3 and not CKA) or (not Y1 and not Y3 and not CKA) or (not Y1 and Y2 and not Y3));
-
-Y3 <= ((Y2 and Y3) or (Y1 and Y2 and not CKA) or (not Y1 and Y3 and CKA));
-
-QA <= (((not Y1 and not Y3 and not CKA) or (Y2 and not Y3 and not CKA) or (not Y1 and Y2 and not CKA) or (not Y1 and Y2 and not Y3) or (not Y1 and not Y2 and Y3 and CKA) or (Y1 and Y2 and Y3 and CKA)) or R5) nor R0;
-
-QB <= (((Y1 and Y2 and CKA) or (Y1 and Y2 and not Y3)) nor (R5 or R0));
-
-QC <= (((Y2 and Y3 and not CKA) or (not Y1 and Y2 and Y3) or (not Y1 and Y3 and CKA)) or R5) nor R0;
+    Y1 <= (( (Y1 and not Y2) or
+     (Y1 and not Y4 and CKA) or 
+     (Y2 and not Y3 and not Y4 and not CKA) or 
+     (Y1 and not Y3 and not Y4) ) and (not R0 and not R5));
+    
+    Y2 <= (( (Y2 and not Y4 and not CKA) or 
+    (CKA and Y1 and Y3 and not Y4) or
+    (Y1 and Y2 and Y3 and not Y4) or
+    (not Y1 and not Y3 and not Y4 and CKA) or
+    (not Y1 and Y2 and not Y3 and not Y4) ) and (not R0 and not R5));
+    
+    Y3 <= ((( (Y3 and not Y4) or
+     (Y1 and not Y2 and Y3) or 
+     (Y1 and not Y2 and Y4 and CKA) or 
+     (not Y1 and not Y2 and Y3 and not CKA) ) and (not R0 and not R5)) or R5);
+     
+    Y4 <= ((( (not Y2 and Y4 and CKA) or
+    (not Y1 and not Y2 and Y3 and Y4) or
+    (Y1 and not Y2 and not Y3 and Y4) or 
+    (Y1 and not Y2 and not Y3 and not CKA) or 
+    (not Y1 and not Y2 and Y3 and not CKA) ) and (not R0 and not R5)) or R5);
+    
+    QA <= ( (not Y1 and not Y2 and Y3) or 
+    (not Y1 and Y2 and Y3 and not Y4) or
+    (not CKA and Y2 and Y3 and not Y4) or
+    (not Y1 and Y3 and not Y4) or
+    (CKA and not Y1 and not Y2 and Y4) );
+    
+    QB <= ( (Y1 and not Y2 and not CKA) or
+    (Y4 and not Y2 and Y1) or 
+    (Y1 and not Y2 and Y3) or
+    (Y1 and Y3 and not Y4 and CKA));
+    
+    QC <= ( (Y1 and not Y4 and CKA) or 
+    (not Y1 and not Y2 and Y4 and CKA) or 
+    (not Y1 and not Y2 and Y3 and not CKA) or 
+    (Y2 and not Y3 and not Y4 and not CKA) or 
+    (Y1 and not Y2 and Y3 and not CKA) or 
+    (not Y1 and not Y2 and Y3 and Y4) or 
+    (Y1 and Y2 and not Y3 and not Y4) or 
+    (Y1 and not Y2 and Y3 and not Y4) );
 
 end Behavioral;
---
+++ project_2/project_2.srcs/sources_1/new/async_strobe.vhd
@@ -0,0 +1,24 @@
+----------------------------------------------------------------------------------
+-- Engineer: Tyler Thompson
+-- Create Date: 11/28/2018 04:14:44 PM
+-- Design Name: 
+-- Module Name: async_strobe - Behavioral
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity async_strobe is
+  Port ( 
+    
+  );
+end async_strobe;
+
+architecture Behavioral of async_strobe is
+
+begin
+
+
+end Behavioral;
