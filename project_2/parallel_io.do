888b3637700ad454a08e283723cf4035fb205a07
Tyler Thompson <tyler.l.thompson@wmich.edu>
2018-11-30 14:36:07 -0500

    almost done

--- project_2/parallel_io.do
+++ project_2/parallel_io.do
@@ -8,36 +8,65 @@ force CE 0 @10ns
 
 force A0 0 @0ns
 force A0 1 @10ns
-force A0 0 @20ns
-force A0 1 @40ns
-force A0 0 @50ns
+force A0 0 @30ns
+force A0 1 @50ns
+force A0 0 @70ns
+force A0 1 @120ns
+force A0 0 @130ns
+force A0 1 @150ns
+force A0 0 @170ns
+force A0 1 @220ns
+force A0 0 @230ns
 
-force STB 0 @0ns
-force STB 1 @20ns
-force STB 0 @30ns
-force STB 1 @40ns
-force STB 0 @60ns
-force STB 1 @70ns
+force STB 1 @0ns
+force STB 0 @40ns
+force STB 1 @50ns
+force STB 0 @80ns
+force STB 1 @90ns
+force STB 0 @180ns
+force STB 1 @190ns
 
 force RD 1 @0ns
 force RD 0 @20ns
-force RD 1 @30ns
-force RD 0 @80ns
-force RD 1 @90ns
+force RD 1 @40ns
+force RD 0 @60ns
+force RD 1 @70ns
+force RD 0 @100ns
+force RD 1 @110ns
+force RD 0 @120ns
+force RD 1 @130ns
+force RD 0 @140ns
+force RD 1 @150ns
+force RD 0 @160ns
+force RD 1 @170ns
+force RD 0 @200ns
+force RD 1 @210ns
+force RD 0 @220ns
+force RD 1 @230ns
+force RD 0 @240ns
+force RD 1 @250ns
+force RD 0 @260ns
 
 force WR 1 @0ns
 force WR 0 @10ns
 force WR 1 @20ns
-force WR 0 @40ns
-force WR 1 @50ns
+force WR 0 @50ns
+force WR 1 @60ns
+force WR 0 @150ns
+force WR 1 @160ns
 
-force D 2#00000000 @0ns
+force D "ZZZZZZZZ" @0ns
 force D 2#00000000 @10ns
 force D "ZZZZZZZZ" @20ns
-force D 2#00000011 @40ns
-force D "ZZZZZZZZ" @50ns
+force D 2#00000011 @50ns
+force D "ZZZZZZZZ" @60ns
+force D 2#00000001 @150ns
+force D "ZZZZZZZZ" @160ns
 
 force P 2#01010100 @0ns
-force P 2#00001111 @50ns
+force P 2#00001111 @70ns
+force P 2#00000000 @90ns
+force P 2#00101101 @170ns
+force P 2#00000000 @190ns
 
-run 160ns
+run 270ns
--- project_2/project_2.srcs/sources_1/new/parallel_io.vhd
+++ project_2/project_2.srcs/sources_1/new/parallel_io.vhd
@@ -23,7 +23,10 @@ architecture Behavioral of parallel_io is
 
     signal control : STD_LOGIC_VECTOR(1 downto 0) := "ZZ";
     signal status : STD_LOGIC_VECTOR(2 downto 0) := "000";
+    signal async_status : STD_LOGIC_VECTOR(1 downto 0) := "00";
     signal data: STD_LOGIC_VECTOR(7 downto 0) := "ZZZZZZZZ";
+    signal read_status : STD_LOGIC := '0';
+    signal QRD : STD_LOGIC := '1';
     signal async_strobe_reset: STD_LOGIC := '1';
     
     component async_strobe
@@ -34,27 +37,43 @@ architecture Behavioral of parallel_io is
     end component;
 
 begin
+    QRD <= RD or CE or A0;
+    
+    
 
     AS: async_strobe port map (
         STB => STB,
-        RD => RD,
+        RD => QRD,
         RESET => async_strobe_reset,
-        IBF => status(0),
-        INTR => status(2)
+        IBF => async_status(0),
+        INTR => async_status(1)
     );
 
-data_bus: process(RD, data, control)
+data_bus: process(RD, STB, A0, data, control, status, read_status, async_strobe_reset)
 begin
     
-    if(RD = '0') then
-        D <= data;
-    elsif (RD = '1') then
-        D <= "ZZZZZZZZ";
+    if(read_status = '1') then
+        D(0) <= status(0);
+        D(1) <= status(1);
+        D(2) <= status(2);
+        D(3) <= '0';
+        D(4) <= '0';
+        D(5) <= '0';
+        D(6) <= '0';
+        D(7) <= '0';
+    elsif (read_status = '0') then
+        if(A0 = '0' and CE = '0' and RD = '1' and async_strobe_reset = '1') then
+            D <= data;
+        elsif(A0 = '0' and CE = '0' and RD = '0' and async_strobe_reset = '1') then
+            D <= P;
+        elsif (RD = '1' and read_status = '0') then
+            D <= "ZZZZZZZZ";
+        end if;
     end if;
     
-    if (control(0) = '1') then
+    if (control(0) = '1' and STB = '0' and RD = '1' and A0 = '0') then
         async_strobe_reset <= '0';
-    elsif (control(0) = '0') then
+    elsif (control(0) = '0' or A0 = '1') then
          async_strobe_reset <= '1';
     end if;
     
@@ -78,32 +97,67 @@ begin
         control <= "00";
     else
         if(WR'event and WR = '0' and A0 = '1' and CE = '0') then
-            control(0) <= D(0); -- mode
-            control(1) <= D(1); -- INTE
+            if ( D(0) = 'X' ) then
+                control(0) <= '0'; -- mode
+            else
+                control(0) <= D(0); -- mode
+            end if;
+             if ( D(1) = 'X' ) then
+                 control(1) <= '0'; -- INTE
+             else
+                 control(1) <= D(1); -- INTE
+             end if;
+            
+            
         end if;
     end if;
 
 end process control_reg;
 
-status_reg: process(A0, RESET, CE, RD, status)
+status_reg: process(A0, RESET, CE, RD, STB, status, async_status, control, async_strobe_reset)
 begin
-    status(1) <= control(1);
+    status(1) <= control(1);-- INTE
     IBF <= status(0);
-    INTR <= status(2);
+    if ( control(1) = '1') then
+        INTR <= status(2);
+    else
+        INTR <= '0';
+    end if;
     
     if (RESET = '1' ) then
+        status(0) <= '0';
         status(1) <= '0';
+        status(2) <= '0';
+--    elsif(control(0) = '1' and A0 = '0' and CE = '0' and RD = '0' and STB = '1'and async_strobe_reset = '1') then
+--        status(0) <= '0';
+--        status(2) <= '0';
     else
         if(RD'event and RD = '0' and A0 = '1' and CE = '0') then
-            data(0) <= status(0); -- IBF
-            data(1) <= status(1); -- INTE
-            data(2) <= status(2); -- INTR
-            data(3) <= '0';
-            data(4) <= '0';
-            data(5) <= '0';
-            data(6) <= '0';
-            data(7) <= '0';
+            read_status <= '1';
         end if;
+        
+        if(A0 = '0') then
+            read_status <= '0';
+        end if;
+        
+        if( (async_status(0)'event) and (async_status(0) = '1') ) then
+            status(0) <= async_status(0);
+        end if;
+        
+        if( (async_status(1)'event) and (async_status(1) = '1') ) then
+            --if ( control(1) = '1') then
+                status(2) <= async_status(1);
+           -- end if; 
+        end if;
+
+        if ( RD'event and RD = '1' and status(2) = '0') then
+            status(0) <= '0'; -- IBF
+        end if;
+        
+        if ( RD'event and RD = '0' and control(0) = '1' and A0 = '0' and CE = '0' and STB = '1'and async_strobe_reset = '1') then
+            status(2) <= '0'; -- INTR
+        end if;
+        
     end if;
 end process status_reg;
 
